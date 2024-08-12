9c17e0b268440faef82b8a537f2264ca902a41a5
Matt1tech <mohammadalbukaai@gmail.com>
2024-08-12 21:54:18 +0800

    update the dating

--- frontend/lib/urls.dart
+++ frontend/lib/urls.dart
@@ -1,7 +1,7 @@
 //emulator
 //final String baseUrl = "http://10.0.2.2:8000";
+
 //physical device
 const String baseUrl = "http://192.168.100.7:8000";
-
-//const String baseUrl = "http://172.17.11.51:8000";
-//const String baseUrl = "http://192.168.0.17:8000";
+//const String baseUrl = "http://192.168.145.17:8000";
+//const String baseUrl = "http://localhost:8000";
