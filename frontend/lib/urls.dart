29fb2ddafd525e8ecd49fffb7496896ed84fa860
Matt1tech <mohammadalbukaai@gmail.com>
2024-08-16 18:33:47 +0800

    push

--- frontend/lib/urls.dart
+++ frontend/lib/urls.dart
@@ -1,7 +1,7 @@
 //emulator
-//final String baseUrl = "http://10.0.2.2:8000";
+final String baseUrl = "http://10.0.2.2:8000";
 
 //physical device
-const String baseUrl = "http://192.168.100.7:8000";
+//const String baseUrl = "http://192.168.100.7:8000";
 //const String baseUrl = "http://192.168.145.17:8000";
-//const String baseUrl = "http://localhost:8000";
+//const String baseUrl = "http://192.168.235.17:8000";
